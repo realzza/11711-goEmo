eb9518c7b5031ecc62e922e64a6d5bd4e5ee29ff
Jiyang Tang <jiyangta@andrew.cmu.edu>
2022-11-11 21:00:03 -0500

    Rearrage directories so that modules are properly imported, add training script

---
+++ train.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=$(pwd)
+
+model_dir=goemotions/bert/cased_L-12_H-768_A-12/
+python goemotions/bert_classifier.py \
+    --bert_config_file $model_dir/bert_config.json \
+    --vocab_file $model_dir/vocab.txt \
+    --output_dir exp
