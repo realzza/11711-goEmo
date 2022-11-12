70a68430d56437aa1825e22788bde886ef03ea7e
Ziang Zhou <ziangz@andrew.cmu.edu>
2022-11-11 23:24:49 -0500

    add dataset files

---
+++ emoDatasets/dataset.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GoEmotionDataset:
+    def __init__(self, texts, labels, tokenizer, max_len):
+        self.texts = texts
+        self.labels = labels
+
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+    
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, index):
+        text = self.texts[index]
+        label = self.labels[index]
+
+        inputs = self.tokenizer.__call__(text,
+                                        None,
+                                        add_special_tokens=True,
+                                        max_length=self.max_len,
+                                        padding="max_length",
+                                        truncation=True,
+                                        )
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
+
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "labels": torch.tensor(label, dtype=torch.long)
+        }
