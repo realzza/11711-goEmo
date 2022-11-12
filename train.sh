8427e41a3c0a366ce83a47fcfe2eeb10e3732a1a
tjysdsg <tjy1018543509@163.com>
2022-11-12 14:29:07 -0500

    Train multilabel classification

--- train.sh
+++ train.sh
@@ -2,6 +2,7 @@ export PYTHONPATH=$(pwd)
 
 model_dir=goemotions/bert/cased_L-12_H-768_A-12/
 python goemotions/bert_classifier.py \
+    --multilabel true \
     --bert_config_file $model_dir/bert_config.json \
     --vocab_file $model_dir/vocab.txt \
     --output_dir exp
