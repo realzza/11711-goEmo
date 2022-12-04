74417e632d1c7a534782242a7259a9212efc42d8
Ziang Zhou <ziangz@andrew.cmu.edu>
2022-12-04 02:30:44 -0500

    adjust training config

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python train.py --db base-baseline --sweep-count 12
-python train.py --db pretrained-emoji-baseline --use-emoji --sweep-count 12
-python train.py --db emoji-unk-baseline --use-emoji --sweep-count 12
-python train.py --db emoji-rand-baseline --use-emoji --sweep-count 12 --emoji-rand-init
+python train.py --db base-baseline --sweep-count 8
+python train.py --db pretrained-emoji-baseline --use-emoji --sweep-count 8
+python train.py --db emoji-unk-baseline --use-emoji --sweep-count 8
+python train.py --db emoji-rand-baseline --use-emoji --sweep-count 8 --emoji-rand-init
