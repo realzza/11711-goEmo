8c087efd5b85a12748dae0833f3ffa62153c7963
Ziang Zhou <ziangz@andrew.cmu.edu>
2022-12-06 21:13:16 -0500

    add log dir flag

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python train.py --db base-baseline --sweep-count 8
-python train.py --db pretrained-emoji-baseline --use-emoji --sweep-count 8
-python train.py --db emoji-unk-baseline --use-emoji --sweep-count 8
-python train.py --db emoji-rand-baseline --use-emoji --sweep-count 8 --emoji-rand-init
+python train.py --db base-baseline-log --sweep-count 1 --logdir out/
+python train.py --db pretrained-emoji-baseline-log --use-emoji --sweep-count 1 --logdir out/
+python train.py --db emoji-unk-baseline-log --use-emoji --sweep-count 1 --logdir out/
+python train.py --db emoji-rand-baseline-log --use-emoji --sweep-count 1 --emoji-rand-init --logdir out/
