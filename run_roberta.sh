3d8b31da395ebc18da644535403b331525e11830
Ziang Zhou <ziangz@andrew.cmu.edu>
2022-12-06 22:08:34 -0500

    script to run roberta

---
+++ run_roberta.sh
@@ -0,0 +1,2 @@
+python train.py --model-type roberta --db roberta-baseline --sweep-count 5 --logdir final_out/
+python train.py --model-type roberta --db roberta-emoji2vec --use-emoji --sweep-count 5 --logdir out/
