f5ab9c622cc279804184fb22621bdea40f2d9c97
Ziang Zhou <ziangz@andrew.cmu.edu>
2022-12-09 01:31:53 -0500

    final running script

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
-python train.py --db base-baseline-log --sweep-count 1 --logdir out/
-python train.py --db pretrained-emoji-baseline-log --use-emoji --sweep-count 1 --logdir out/
-python train.py --db emoji-unk-baseline-log --use-emoji --sweep-count 1 --logdir out/
-python train.py --db emoji-rand-baseline-log --use-emoji --sweep-count 1 --emoji-rand-init --logdir out/
+python train.py --model-type bert --db final-bert-ekman-base --sweep-count 4 --logdir final_idea/ --task ekman
+python train.py --model-type bert --db final-bert-ekman-emoji2vec --use-emoji --sweep-count 4 --logdir final_idea/ --task ekman
+python train.py --model-type bert --db final-bert-ekman-emoji-text-align --use-emoji --text-align --sweep-count 4 --logdir final_idea/ --task ekman
+python train.py --model-type bert --db final-bert-sentiment-base --sweep-count 4 --logdir final_idea/ --task sentiment
+python train.py --model-type bert --db final-bert-sentiment-emoji2vec --use-emoji --sweep-count 4 --logdir final_idea/ --task sentiment
+python train.py --model-type bert --db final-bert-sentiment-text-align --text-align --use-emoji --sweep-count 4 --logdir final_idea/ --task sentiment
+# python train.py --model-type roberta --db roberta-sentiment --sweep-count 4 --logdir final_out/ --task sentiment
+# python train.py --model-type roberta --db roberta-ekman --sweep-count 2 --logdir final_out/ --task ekman
+# python train.py --model-type roberta --db roberta-sentiment-emoji2vec --use-emoji --sweep-count 2 --logdir final_out/ --task sentiment
+# python train.py --model-type roberta --db roberta-ekman-emoji2vec --use-emoji --sweep-count 2 --logdir final_out/ --task ekman
+
+python train.py --model-type bert --db final-bert-baseline --sweep-count 4 --logdir final_idea/
+python train.py --model-type bert --db final-bert-emoji2vec --use-emoji --sweep-count 4 --logdir final_idea/
+python train.py --model-type bert --db final-bert-emoji-text-align --use-emoji --text-align --sweep-count 4 --emoji-rand-init --logdir final_idea/
+# python train.py --model-type roberta --db roberta-baseline --sweep-count 8 --logdir final_out/
+# python train.py --model-type roberta --db roberta-emoji2vec --use-emoji --sweep-count 8 --logdir final_out/
