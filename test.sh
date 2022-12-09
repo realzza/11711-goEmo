ee17c6438f3af6d08ff87ab68cbd265e42a79903
tjysdsg <tjy1018543509@163.com>
2022-12-08 22:43:55 -0500

    Run test on all configurations

--- test.sh
+++ test.sh
@@ -1,5 +1,58 @@
+# GOEMOTION TAXONOMY
 python test.py \
-  --db=pretrained-emoji-baseline-test \
+  --db=bert-baseline-test \
+  --checkpoint=final_out/bert-cased-baseline/best_model.pt \
+  --model-type=bert || exit 1
+
+python test.py \
+  --db=bert-emoji2vec-test \
   --use-emoji \
   --checkpoint=final_out/bert-cased-emoji2vec/best_model.pt \
   --model-type=bert || exit 1
+
+python test.py \
+  --db=roberta-test \
+  --checkpoint=final_out/roberta-baseline/best_model.pt \
+  --model-type=roberta || exit 1
+
+# TODO
+#  python test.py \
+#    --db=roberta-emoji2vec-test \
+#    --use-emoji \
+#    --checkpoint=final_out/roberta-emoji2vec/best_model.pt \
+#    --model-type=roberta || exit 1
+
+# TODO
+#  python test.py \
+#    --db=bert-emoji-rand-test \
+#    --use-emoji \
+#    --emoji-rand-init \
+#    --checkpoint=final_out/bert-cased-emoji-rand/best_model.pt \
+#    --model-type=bert || exit 1
+
+# EKMAN
+python test.py \
+  --db=bert-ekman-test \
+  --task=ekman \
+  --checkpoint=final_out/bert-ekman/best_model.pt \
+  --model-type=bert || exit 1
+
+python test.py \
+  --db=bert-ekman-emoji2vec-test \
+  --task=ekman \
+  --use-emoji \
+  --checkpoint=final_out/bert-ekman-emoji2vec/best_model.pt \
+  --model-type=bert || exit 1
+
+python test.py \
+  --db=roberta-ekman-test \
+  --task=ekman \
+  --checkpoint=final_out/roberta-ekman/best_model.pt \
+  --model-type=roberta || exit 1
+
+python test.py \
+  --db=roberta-ekman-emoji2vec-test \
+  --task=ekman \
+  --use-emoji \
+  --checkpoint=final_out/roberta-ekman-emoji2vec/best_model.pt \
+  --model-type=roberta || exit 1
